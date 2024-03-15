3128ce74a97a939b471989c50545a3f3d0362a0b
danny6543211 <danny6543211@gmail.com>
2024-03-15 22:37:00 +0800

    fix bugs

--- killRJ.py
+++ killRJ.py
@@ -5,22 +5,26 @@ import time
 import ini
 
 
-def find_pid(process_name):
+def find_pid(process_name: str):
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == process_name:
             return proc.info['pid']
     return None
 
-
-def main():
-    subprocess.Popen(ini.RJ_client_path)
-    print('Please wait for three seconds...')
-    targets_kill = ['8021x.exe', 'suservice.exe', '8021x.exe']
-    for t in targets_kill:
-        time.sleep(1)
+def kill(target_kill: list):
+    for t in target_kill:
+        time.sleep(0.5)
         pid = find_pid(t)
         process = psutil.Process(pid)
         process.kill()
+
+def main():
+    print('Please wait for seconds...')
+    
+    target_kill = ['8021x.exe', 'suservice.exe', '8021x.exe']
+    subprocess.Popen(ini.RJ_client_path)
+    time.sleep(3)
+    kill(target_kill)
 
 
 if __name__ == '__main__':
